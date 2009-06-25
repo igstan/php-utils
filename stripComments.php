d6a0c2a7840f93aedbda3aaad96b44d693be6376
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-06-25 20:33:58 +0300

    Added function for stripping PHP comments
    I don't know how useful this function is, but someone needed it once, so
    I thought it may serve others as well. At least as an example of how to
    tokenize and process PHP source code.

---
+++ stripComments.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Removes comment declarations from a string of PHP source code.
+ *
+ * @author  Ionut G. Stan
+ * @license See LICENSE in the root of the project
+ * @param   string $source PHP source code
+ * @return  string
+ */
+function stripComments($source) {
+    $strippedSource = '';
+    $commentTokens  = array(T_COMMENT);
+
+    if (defined('T_DOC_COMMENT')) $commentTokens[] = T_DOC_COMMENT; // PHP 5
+    if (defined('T_ML_COMMENT'))  $commentTokens[] = T_ML_COMMENT;  // PHP 4
+
+    $tokens = token_get_all($source);
+
+    foreach ($tokens as $token) {
+        if (is_array($token)) {
+            if (in_array($token[0], $commentTokens)) {
+                continue; // skip comment declarations
+            }
+
+            $token = $token[1];
+        }
+
+        $strippedSource .= $token;
+    }
+
+    return $strippedSource;
+}
